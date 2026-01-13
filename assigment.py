43be4f19fd09d56735acfb9db955163d9780211a
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-01-13 09:27:13 +0530

    Add files via upload

---
+++ assigment.py
@@ -0,0 +1,56 @@
+"""x="hello atimya university"
+print(x)"""
+
+"""a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+sum=a+b
+print("sum=",sum)"""
+
+"""num =int(input("enter a number:"))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("odd number")"""
+    
+    
+"""year=int(input("Enter a year:"))
+if year % 4==0:
+    print("leap year")
+else:
+     print("not a leap year")"""
+     
+     
+"""import math
+print(math.pi)"""
+
+
+"""PI = 3.14
+print("PI=",PI)"""
+
+"""num = float(input("enter a number:"))
+print("Sqaure=",num +num)"""
+
+"""r = float(input("Enter radius:"))
+area = 3.14 * r * r
+print("Area of circle=",area)"""
+
+
+"""x = 10
+print(type(x))"""
+
+"""import math  
+print(math.sqrt(16))
+print(math.factorial(5))"""
+
+"""a = int(input("enter base:"))
+b = int(input("enter the power:"))
+print(a ** b)"""
+
+"""num = int(input("Enter a number:"))
+ 
+if num > 0:
+     print("positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("zero")"""    
