942ec68e326864c88786eea5bbfca48a312fd309
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-01-20 09:27:18 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,48 @@
+"""P = float(input("Enter principal amount: "))
+R = float(input("Enter rate of interest (%): "))
+T = float(input("Enter time (years): "))
+
+SI = (P * R * T) / 100
+
+print("Simple Interest:", SI)"""
+
+
+"""num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("The maximum number is:", max(num1, num2))"""
+
+"""for i in range(1, 6):
+    print(i)"""
+
+"""s = input ("Enter a string:")
+print("Length of string:", len(s))"""
+
+"""print("welcome to python programming!")"""
+
+"""s = input("Enter a string:")
+print("Frist character:",s[0])"""
+
+
+"""s = input("Enter a string:")
+print("last character:",s[-1])"""
+
+number = int(input("Enter a number: "))
+
+"""if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")"""
+
+
+"""a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+sum = a + b + c
+print("Sum is:", sum)"""
+
+"""name = input("Enter your name:")
+print("hello",name)"""
