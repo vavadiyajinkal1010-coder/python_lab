da7a105710effb1cc13be9d90ba4d1f822a9c298
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-01-27 09:17:08 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,49 @@
+#Arithmetic operator:
+"""a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)"""
+
+#Assignment operator:
+"""x=20
+print(x)"""
+
+#Unary minus
+"""a=25
+print(-a)"""
+
+#Relational operators:
+"""x=15
+z=10
+print(x>z)
+print(x==z)"""
+
+#Logical operators:
+"""a=True
+b=False
+print(a and b)
+print(a or b)
+print(not a)"""
+
+#Boolean operators
+"""is_pass=True
+print(is_pass)"""
+
+#Bitwise operators
+"""a=5
+b=3
+print(a&b)
+print(a/b)"""
+
+#Membership operators
+"""numbers= [1,2,3,4]
+print(2 in numbers)
+print(5 not in numbers)"""
+
+#Identity operators:
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
