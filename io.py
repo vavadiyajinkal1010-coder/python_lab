3d94daef2daa1e8b3452cb251b574a60dea7155c
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-01-31 08:26:54 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,13 @@
+#input
+"""print("Welcome to python programming")
+print("Myself jinkal vavadiya")"""
+
+#output
+"""name=input("Enter your name:")
+print("Hello",name)
+rollno=input ("Enter your roll no:")
+print("Hello",rollno)"""
+
+#output
+price=float(input("Enter price:"))
+print("Price entered:",price)
