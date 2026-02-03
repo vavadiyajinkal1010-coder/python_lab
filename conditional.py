510e35e24ae8d44c41e85e5fc1488d4465fe7808
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-02-03 09:13:25 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,11 @@
+"""age = int(input("Enter your age:"))
+if age >= 18:
+  print("yes you are eligible for vote")
+else:
+  print("no you are not eligible for vote")"""
+
+marks = int(input("Enter your marks:"))
+if marks >= 20:
+  print("yes you are pass")
+else:
+  print("no you are not pass")
