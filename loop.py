3922fc639e0fa41a4facea93f6f3f2701f87d404
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-02-24 08:09:44 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,33 @@
+# print number from 1 to 21
+"""for i in range (1,21):
+  print(i)"""
+
+# print even numbers from 1 to 20 for i in range(1,21)
+"""for i in range (1,21): 
+ if i % 2 == 0: 
+  print(i)"""
+
+# print odd numbers from 1 ro 15 for i in range(1,16)
+"""for i in range (1,16):
+    if i % 2 !=0:
+        print(i)"""
+
+# print table of 5 for i in range(1,11)
+"""for i in range (1,11):
+ print("5 x ",i,"=",5 * i )"""
+
+#print character of a string
+"""name="Atmiya"
+for letter in name:
+    print(letter)"""
+
+#sum of numbers from 1 to 5
+"""total=0
+for i in range(1,6):
+    total = total + i
+print("sum is:",total)"""
+
+# print list elements 
+"""numbers = [10,20,30,40]
+for n in numbers:
+    print(n)"""
