a35f0658dc4bad9ae5cb90fcd0c6a6a6622299c3
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-02-24 08:20:30 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,21 @@
+# print numbers from 1 to 10
+"""i = 1
+while i<=10:
+    print(i)
+    i = i+1"""
+
+# sum of first n natural numbers
+"""n= int(input("Enter n :"))
+i=1
+s=0
+while i<=n:
+     s=s+i
+     i=i+1
+     print("sum =",s)"""
+
+#table of a number
+num = int(input("Enter number:"))
+i = 1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i = i+1
