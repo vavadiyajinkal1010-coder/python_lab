7f018ccddf49f473d68b9446fe0a8caefffa0e12
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-03-10 09:28:28 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,19 @@
+#positive indexing
+"""from array import array
+arr= array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])"""
+
+#negative indexing 
+"""from array import array
+arr= array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])"""
+
+#modify
+from array import array
+arr= array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)
