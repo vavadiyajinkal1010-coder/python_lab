0a15cc6f3fbcd1f297116dc63fbbff846b0611c6
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-03-31 08:39:41 +0530

    Add files via upload

---
+++ write.py
@@ -0,0 +1,13 @@
+"""f=open("one.txt","w")
+f.write("Hello students\n")
+f.write("Welcome to file handling.\n")
+f.write("Learning is fun!\n")
+f.close()"""
+
+"""f=open("one.txt","w")
+f.write("New content only.\n")
+f.close()"""
+
+f=open("one.txt","w")
+f.write("This line is added at the end.\n")
+f.close()
