b8634912e626567dd92d75490dcf009a4cb0ba41
vavadiyajinkal1010-coder <vavadiyajinkal1010@gmail.com>
2026-04-04 09:12:02 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,7 @@
+def student_info(name,age=18,course="bsc.it"):
+    print("name",name)
+    print("age",age)
+    print("course",course)
+student_info("mishri")
+student_info("malvi",20)
+student_info("riya",19,"bsc.it")
